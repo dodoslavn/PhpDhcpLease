c5cde59fc20e87fc6978aa9cff63a2786283bfd6
Dodoslav Novak <dodoslavn@gmail.com>
2024-07-18 22:01:40 +0200

    show in table

--- web/index.php
+++ web/index.php
@@ -22,7 +22,36 @@ foreach ($config->configuration->lease_files as $lease_file)
 	{
 	$command = $binary." --parsable --lease ".$lease_file;
 	exec($command, $output, $return_var);
-	print_r($output);
+	foreach($output as $one_lease)
+		{
+		$exploded = explode(" ", $one_lease);
+		echo '
+<table>
+	<tr>
+		<tr>'.$exploded[0].'</td>
+		<tr>'.$exploded[1].'</td>
+	</tr>
+	<tr>
+		<tr>'.$exploded[2].'</td>
+		<tr>'.$exploded[3].'</td>
+	</tr>
+	<tr>
+		<tr>'.$exploded[4].'</td>
+		<tr>'.$exploded[5].'</td>
+	</tr>
+	<tr>
+		<tr>'.$exploded[6].'</td>
+		<tr>'.$exploded[7].'</td>
+	</tr>
+	<tr>
+		<tr>'.$exploded[8].'</td>
+		<tr>'.$exploded[9].'</td>
+	</tr>
+	<tr>
+		<tr>'.$exploded[10].'</td>
+		<tr>'.$exploded[11].'</td>
+	</tr>';
+		}
 	}
 
 
